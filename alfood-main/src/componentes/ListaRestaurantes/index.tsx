f4ef0ca8003bcaa7420d1db9c7e6d1486b5056ad
latec-devluiz <luiz@latecsolucoes.com.br>
2026-09-01 18:56:00 -0300

    Atualiza lista de restaurantes 4

--- alfood-main/src/componentes/ListaRestaurantes/index.tsx
+++ alfood-main/src/componentes/ListaRestaurantes/index.tsx
@@ -3,16 +3,20 @@ import { useEffect, useState } from 'react';
 import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
+import { IPaginacao } from '../../interfaces/IPaginacao';
 
 const ListaRestaurantes = () => {
 
-const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
+  const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
+
+  const [proximapagina, setProximaPagina] = useState('')
 
   useEffect(() => {
     // obter restaurantes da API
-    axios.get('/api/v1/restaurantes/')
+    axios.get < IPaginacao<IRestaurante>('/api/v1/restaurantes/')
       .then(resposta => {
-        setRestaurantes(resposta.data.results)              
+        setRestaurantes(resposta.data.results)
+        setproximaPagina(resposta.data.next)
       })
       .catch(erro => {
         console.log(erro)
@@ -21,8 +25,11 @@ const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
   }, [])
 
   return (<section className={style.ListaRestaurantes}>
-    <h1>Os restaurantes mais <em>bacanas</em>!</h1>
+    <h1>Os restaurantes mais <em>bacanas 4</em>!</h1>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
+    {proximaPagina && <button>
+      ver mais
+    </button>}
   </section>)
 }
 
